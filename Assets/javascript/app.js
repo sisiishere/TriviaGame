7174ac00153bb670b42d5706b45d5c66cab91f6b
sisiishere <set38246@marymount.edu>
2019-06-19 16:58:11 -0400

    modify

---
+++ Assets/javascript/app.js
@@ -0,0 +1,168 @@
+varArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",]
+
+varResults = [];
+varWins = 0;
+varLosses = 0;
+varTimer = 60000;
+
+var reset = function(){
+    document.getElementById("Wins:").innerHTML = Wins;
+
+    document.getElementById("Losses:").innerHTML = Losses;
+
+    document.getElementById("Results:").innerHTML = Array;
+
+    document.getElementById("Clock").innerHTML = Timer;
+
+}
+
+var displayResults = function() {
+    document.getElementById('Wins:').innerHTML = 'Wins: ' + win;
+    document.getElementById('Losses:').innerHTML = 'Losses: ' + loss;
+    document.getElementById('Results:').innerHTML = 'Results: ' + Array;
+  };
+
+  var Timer = setTimeout( "setTimeFunction( )", 1000-- ); 
+
+  ("soak").onclick = function(){Wins++}; 
+
+  ("notturnitout").onclick = function(){Losses++}; 
+
+  ("thankyou").onclick = function(){Wins++}; 
+
+  ("nooooo").onclick = function(){Losses++}; 
+
+  ("underestimate").onclick = function(){Wins++}; 
+
+  ("shade").onclick = function(){Losses++}; 
+
+  ("robbed").onclick = function(){Wins++}; 
+
+  ("foiled").onclick = function(){Losses++};
+  
+  ("winna").onclick = function(){Wins++}; 
+
+  ("bttm3").onclick = function(){Losses++}; 
+
+  ("haha").onclick = function(){Wins++}; 
+
+  ("heartbreak").onclick = function(){Losses++}; 
+
+  ("body").onclick = function(){Wins++}; 
+
+  ("what").onclick = function(){Losses++}; 
+
+  ("pinche").onclick = function(){Wins++}; 
+
+  ("iwashungry").onclick = function(){Losses++}; 
+
+  ("mocha").onclick = function(){Wins++}; 
+
+  ("wtf").onclick = function(){Losses++}; 
+
+  ("prettymonster").onclick = function(){Wins++}; 
+
+  ("dfwm").onclick = function(){Losses++}; 
+
+console.log("Quest 1");
+
+if ("#MonétXChange") {
+    Wins++
+    console.log("Soak it up!")
+}
+
+else {
+    Losses++
+    console.log("You're just not...turning it out!")
+}
+
+if ("#ErikaKlash") {
+    Wins++
+    console.log("THANK YOU!!!")
+}
+
+else {
+    Losses++
+    console.log("NOOOOOO!!!")
+}
+
+if ("#Amadiva") {
+    Wins++
+    console.log("Don't underestimate a babe like me!")
+}
+
+else {
+    Losses++
+    console.log("It's okay, you will get it right next time!")
+}
+
+if ("#ManilaLuzon") {
+    Wins++
+    console.log("Finally, I am no longer robbed!")
+}
+
+else {
+    Losses++
+    console.log("Robbed again!")
+}
+
+if ("#Loris") {
+    Wins++
+    console.log("Yes! A win for once and for all!")
+}
+
+else {
+    Losses++
+    console.log("Not again!")
+}
+
+if ("#KandyZyanide") {
+    Wins++
+    console.log("Hahahahah!")
+}
+
+else {
+    Losses++
+    console.log("I'm heartbroken.")
+}
+
+if ("#A'keriaC.Davenport") {
+    Wins++
+    console.log("I am the body!")
+}
+
+else {
+    Losses++
+    console.log("What's going on?")
+}
+
+if ("#PincheQueen") {
+    Wins++
+    console.log("I am that queen!")
+}
+
+else {
+    Losses++
+    console.log("BALENCIAGA!!!")
+}
+
+if ("#MochaDiva") {
+    Wins++
+    console.log("I maybe pretty, but I got gunz for days!")
+}
+
+else {
+    Losses++
+    console.log("WHAT?!")
+}
+
+if ("#Foxie") {
+    Wins++
+    console.log("That's how the pretty monster does it!")
+}
+
+else {
+    Losses++
+    console.log("Don't mess with me!")
+
+}
