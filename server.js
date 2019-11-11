a13d151a0f0a5b9a17475fdc53c94168bd28bf87
sisiishere <set38246@marymount.edu>
2019-11-10 21:17:27 -0500

    updating changes in server file

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 // Starts the server to begin listening
